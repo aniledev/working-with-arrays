14af90834437f1320c02dddb4b5c5981ff6b3602
aniledev <elinamcgill@gmail.com>
2021-02-17 00:22:53 -0500

    REFACTOR: allocate more "memory blocks" than needed to aoid resizing every time you push some data

--- build-array.js
+++ build-array.js
@@ -4,12 +4,18 @@ class Array {
   // this would create an empty array with 0 "blocks of memory"
   constructor() {
     this.length = 0;
+    // capacity indicates how many items you can hold without needing to resize
+    this._capacity = 0;
     this.prt = memory.allocate(this.length);
   }
 
   // push to end of array -- increase the amount of memory blocks
   push(value) {
-    this._resize(this.length + 1);
+    // array should be capable of holding more than than it actually has; if memory blocks >= capacity, then resize
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+
     // then set the value of the new memory blocks
     memory.set(this.pointer + this.length, value);
     this.length++;
@@ -18,10 +24,19 @@ class Array {
   _resize(size) {
     const oldPointer = this.pointer;
     this.pointer = memory.allocate(size);
+
     if (this.pointer === null) {
       throw new Error("Out of memory");
     }
+
     memory.copy(this.pointer, oldPointer, this.length);
     memory.free(oldPointer);
+    this._capacity = size;
   }
+    
+    
+    
 }
+
+// triple the size of memory that is allocated
+Array.SIZE_RATIO = 3;
