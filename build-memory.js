66ffabce8fa7b4849102a8ebdabe99dd4a140a06
aniledev <elinamcgill@gmail.com>
2021-02-28 22:29:01 -0500

    FEAT: write pseudocode for necessary methods in Memory class

---
+++ build-memory.js
@@ -0,0 +1,21 @@
+// initialize a Memory class
+class Memory {
+  /* write constructor method for constructing new objects from the class; the constructor method 
+    should have 2 properties: memory and head set to a value of 0  */
+  constructor() {
+    this.memory = new Float64Array(1024);
+    this.head = 0;
+  }
+
+  // when creating an array, a contiguous block of memory must be created or reserved; this method can be declared as allocate
+
+  // the set method is used to set teh value stored at a certain memory block
+
+  // the get() method is used to retrieve a value stored at a certain memory block
+
+  // the free() method is used to free up a block of memory that was reserved using the allocate method
+    
+    
+}
+
+//
